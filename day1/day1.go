d312da2a0cfd77c1ef4a6fcdb41fa5af391c3a76
AzureAD\LukasBentele <lukas.bentele@drjve.de>
2024-12-03 10:43:43 +0100

    day1B

--- day1/day1.go
+++ day1/day1.go
@@ -134,6 +134,37 @@ func findDifferencesBetweenSlices(l []string, r []string) ([]int, error) {
 	return diff, nil
 }
 
+func getCountOfNumbers(s []string) (map[string]int, error) {
+	if len(s) == 0 {
+		return map[string]int{}, nil
+	}
+	var result map[string]int = make(map[string]int)
+	for _, ss := range s {
+		// clean up ss before using it, there are carriage returns and shit in there which leads to nasty bugs..
+		ss = stripTrailingNewlines(ss)
+		count, ok := result[ss]
+		if ok {
+			result[ss] = count + 1
+		} else {
+			result[ss]++
+		}
+
+	}
+	return result, nil
+}
+
+func getSimilarityScore(str string, counts map[string]int) (int, error) {
+	count, ok := counts[str]
+	if !ok {
+		return 0, nil
+	}
+	value, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, err
+	}
+	return count * value, nil
+}
+
 func A() {
 	fmt.Print("running day 1A.\n")
 	puzzleInput, err := readFileToString("inputA.txt")
@@ -162,4 +193,28 @@ func A() {
 	}
 	sumString := strconv.Itoa(sum)
 	fmt.Printf("The overall sum of differences is: %s\n", sumString)
+}
+
+func B() {
+	fmt.Print("running day 1B.\n")
+	puzzleInput, err := readFileToString("inputA.txt")
+	if err != nil {
+		panic(err)
+	}
+	leftInput, rightInput := getListsFromInput(puzzleInput)
+
+	countsOfNumsInRight, err := getCountOfNumbers(rightInput)
+	if err != nil {
+		panic(err)
+	}
+	var totalScore int = 0
+	for _, l := range leftInput {
+		score, err := getSimilarityScore(stripTrailingNewlines(l), countsOfNumsInRight)
+		if err != nil {
+			panic(err)
+		}
+		totalScore += score
+	}
+	totalScoreString := strconv.Itoa(totalScore)
+	fmt.Printf("The overall sum of similarity scores is: %s\n", totalScoreString)
 }
