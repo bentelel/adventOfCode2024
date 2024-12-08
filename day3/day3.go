a3c4188dbb3a40399214426a98304f41e38d99da
AzureAD\LukasBentele <lukas.bentele@drjve.de>
2024-12-08 20:23:27 +0100

    flashed out logic for part B

--- day3/day3.go
+++ day3/day3.go
@@ -21,26 +21,34 @@ import (
 // inputs are only 1 to 3 digits long!
 
 // naive plan:
-//  split input by "mul(", p.e. input xmul(1,2)_mul(§3,3)__mul( 2, 3)__7
-//  [x 1,2)_ $3,3)__  2,3)__7]
-//  check each entry if it contains ")", if not: discard
-//  then split each element by ) and take the first element
-//  [1,2 $3,3  2,3]
-//  check input if it contains a ",", if not, discard
-//  [1,2 $3,3  2,3]
-//  split input by ","
-//  [[1 2] [$3 3] [ 2 3]]
-//  check each subslice for len()==2 if it has more, discard
-//  check each element in each slice if it is int with at max 3 digits and at least 1 digit.
+//
+//	split input by "mul(", p.e. input xmul(1,2)_mul(§3,3)__mul( 2, 3)__7
+//	[x 1,2)_ $3,3)__  2,3)__7]
+//	check each entry if it contains ")", if not: discard
+//	then split each element by ) and take the first element
+//	[1,2 $3,3  2,3]
+//	check input if it contains a ",", if not, discard
+//	[1,2 $3,3  2,3]
+//	split input by ","
+//	[[1 2] [$3 3] [ 2 3]]
+//	check each subslice for len()==2 if it has more, discard
+//	check each element in each slice if it is int with at max 3 digits and at least 1 digit.
+const (
+	LOWERBOUND    int    = 1
+	UPPERBOUND    int    = 999
+	MUL           string = "mul("
+	RIGHT_BRACKET string = ")"
+	COMMA         string = ","
+	DONT          string = "don't()"
+	DO            string = "do()"
+)
 
 func A() {
-	lowerBound := 1
-	upperBound := 999
 	input, err := utils.ReadFileToString("input.txt", "day3")
 	if err != nil {
 		panic(err)
 	}
-	overallResult, err := getSumOfMuls(input, lowerBound, upperBound)
+	overallResult, err := getSumOfMuls(input, LOWERBOUND, UPPERBOUND)
 	if err != nil {
 		panic(err)
 	}
@@ -65,21 +73,50 @@ func B() {
 	//    output 1: mul(1,2)__mul(2,3)__
 	//    output 2: [mul(5,6)__mul(2,4)_ _mul(1,2)] > discard left > _mul(1,2)
 	//  then run logic from A for all remaining input parts
+	input, err := utils.ReadFileToString("input.txt", "day3")
+	if err != nil {
+		panic(err)
+	}
+	var input_split_by_dont []string = strings.Split(input, DONT)
+	if len(input_split_by_dont) == 0 {
+		panic("Input is empty.. what")
+	}
+	firstBlock := input_split_by_dont[0]
+	var input_only_dos []string
+	input_only_dos = append(input_only_dos, firstBlock)
+	for _, inp := range input_split_by_dont[1:] {
+		input_split_by_do := strings.Split(inp, DO)
+		if len(input_split_by_do) > 2 {
+			panic("assumption that do and dont alternate seems to be wrong")
+		}
+		// discard first part and only use second part
+		input_only_dos = append(input_only_dos, input_split_by_do[1])
+	}
+	var overallSum int = 0
+	for _, inp := range input_only_dos {
+		result, err := getSumOfMuls(inp, LOWERBOUND, UPPERBOUND)
+		if err != nil {
+			panic(err)
+		}
+		overallSum += result
+	}
+
+	fmt.Printf("The overall result is: %d\n", overallSum)
 }
 
 func getSumOfMuls(input string, lowerBound int, upperBound int) (int, error) {
-	var muls []string = strings.Split(input, "mul(")
+	var muls []string = strings.Split(input, MUL)
 	muls_temp := []string{}
 	for _, m := range muls {
-		if strings.Contains(m, ")") {
-			muls_temp = append(muls_temp, strings.Split(m, ")")[0])
+		if strings.Contains(m, RIGHT_BRACKET) {
+			muls_temp = append(muls_temp, strings.Split(m, RIGHT_BRACKET)[0])
 		}
 	}
 	muls = muls_temp
 	var factors [][]string
 	for _, m := range muls {
-		if strings.Contains(m, ",") {
-			f := strings.Split(m, ",")
+		if strings.Contains(m, COMMA) {
+			f := strings.Split(m, COMMA)
 			factors = append(factors, f)
 		}
 	}
